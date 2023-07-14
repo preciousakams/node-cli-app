033fd03a389e4904397bd06365b6505ddbfb8cff
Precious Akams <preciousakams@yahoo.com>
2023-07-14 20:16:50 +0100

    project working as expected

--- bin/index.js
+++ bin/index.js
@@ -15,12 +15,18 @@ inquirer
     {
       type: "input",
       name: "name",
-      message: "what's the file name (include the file part e.g. `/Documents/textfile.csv`)?"
+      message: "what's the file name (include the file's location e.g. `/Documents/textfile.csv`)?"
+    },
+    {
+      type: "input",
+      name: "newFileLocation",
+      message: "What's the location for the sorted document ( e.g. `/Documents`)?"
     }
   ])
   .then(answers => {
     // console.log(`${answers["name"]}`);
     const file = answers["name"];
+    const newFileLocation = answers["newFileLocation"];
       fs.copyFile(file, `./file.csv`, (err) => {
         if (err) {
           throw err;
@@ -31,8 +37,17 @@ inquirer
     
       const data= d3.csvParseRows(fs.readFileSync(file, 'utf8'))
       const flatRollup = d3.flatRollup(data, v => d3.sum(v, d =>  d[2]), d => d[0], d => d[1])
-      console.log(flatRollup);
-
+      const newCsvFile = d3.csvFormat(flatRollup);
+          console.log(newCsvFile);
+      fs.writeFile(`${newFileLocation}/newFile.csv`, newCsvFile, (err) => {
+        if (err) {
+          throw err;
+        } 
+        else {
+          console.log('file was written to destination');
+        }
+      }
+      );
         
 
 
